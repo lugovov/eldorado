ce5d2fc5d1a343bc895154003c361a52359d3ee7
Alexander <git@lugovov.ru>
2020-05-16 18:22:12 +0300

    add meta

---
+++ market.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Eldorado Market
+// @namespace    http://eldorado.botva.ru/
+// @version      0.1.1
+// @downloadURL  https://github.com/lugovov/eldorado/raw/master/market.user.js
+// @updateURL    https://github.com/lugovov/eldorado/raw/master/market.meta.js
+// @description  try to take over the world!
+// @author       me
+// @match        https://eldorado.botva.ru/
+// @grant        none
+// ==/UserScript==
