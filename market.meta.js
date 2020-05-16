6c9a6f4a43cab2a90ab1982ee838c8df10f67d7a
Alexander <lugovov@users.noreply.github.com>
2020-05-16 21:16:31 +0300

    fix

--- market.meta.js
+++ market.meta.js
@@ -1,15 +1,13 @@
 // ==UserScript==
 // @name         Eldorado Market
 // @namespace    http://eldorado.botva.ru/
-<<<<<<< ours
 // @version      0.1.3
-=======
-// @version      0.1.2
->>>>>>> theirs
 // @downloadURL  https://github.com/lugovov/eldorado/raw/master/market.user.js
 // @updateURL    https://github.com/lugovov/eldorado/raw/master/market.meta.js
 // @description  try to take over the world!
 // @author       me
 // @match        https://eldorado.botva.ru/
-// @grant        none
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @run-at      document-start
 // ==/UserScript==
